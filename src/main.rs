319d3718c9881c93d39278efc41aa97378c0876c
hota1024 <hotalog@hota1024.com>
2020-07-05 20:42:11 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,25 @@ fn main() {
         .exit_on_esc(true)
         .build()
         .unwrap();
+
+    let mut x = 0.0;
+    let mut y = 0.0;
+    let size = 64.0;
+
     while let Some(event) = window.next() {
         window.draw_2d(&event, |context, graphics, _device| {
             clear([1.0; 4], graphics);
             rectangle(
                 [1.0, 0.0, 0.0, 1.0], // red
-                [0.0, 0.0, 100.0, 100.0],
+                [x - size / 2.0, y - size / 2.0, size, size],
                 context.transform,
                 graphics,
             );
         });
+
+        if let Some(cursor) = event.mouse_cursor_args() {
+            x = cursor[0];
+            y = cursor[1];
+        }
     }
 }
